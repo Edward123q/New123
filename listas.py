23e63fc336ff1200d7f7826f0c6041cfb7e653f9
Edward123q <134305863+Edward123q@users.noreply.github.com>
2023-05-22 12:33:45 -0400

    lista
    lista

---
+++ listas.py
@@ -0,0 +1,10 @@
+# listas
+listas = [100, "a".true, 2.5]
+print(listas)
+print("valor del indice1",listas[1])
+# indice   1  2  3  4
+lista2 = [10,20,30,40]
+print("valor del indice1",lista2[1])
+print("valor del indice2",len lista2)
+print("valor del indice2",lista2[-1])
+print("valor del indice2",lista2[-2]) # es el ultimo elemento
